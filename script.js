415eae6fc414c1864def53aae3028918c5c17135
Daniel J. Crosby <danieljonathancrosby@gmail.com>
2018-04-30 16:01:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-document.getElementbyID("topNavBar").getElementsByClassName("coming").addEventListener("click", comingSoon);
-    
-    function comingSoon() {
-        alert("Coming Soon");
-}
+document.getElementById("coming").addEventListener("click", function comingSoon() {
+ 
+    alert("Coming Soon");
 
-/*
-function comingSoon() {
-    var popup = document.getELementbyID("topNavBar");
-    popup.classList.toggle("show");
-}
-
-
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
-
-*/
+});
